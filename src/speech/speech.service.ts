9830397bb883d578c9ec96738a758c1f26cf5a0b
glebmark <rulk_eve@protonmail.com>
2023-05-03 23:06:35 +0400

    refactoring of db in speech

--- src/speech/speech.service.ts
+++ src/speech/speech.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-const TextToSpeech = require('@google-cloud/text-to-speech');
+import TextToSpeech from '@google-cloud/text-to-speech';
 
 import { FileFormat } from '../resource/entities/resource.entity';
 import { LanguageType, Title } from '../bot/entities/title.entity';
@@ -14,7 +14,22 @@ const voiceSettings = {
         languageCode: 'en-US', 
         name: "en-US-Wavenet-J" 
     }
+
+    enum SsmlVoiceGender {
+        SSML_VOICE_GENDER_UNSPECIFIED = 0,
+        MALE = 1,
+        FEMALE = 2,
+        NEUTRAL = 3
+    }
     
+    enum AudioEncoding {
+        AUDIO_ENCODING_UNSPECIFIED = 0,
+        LINEAR16 = 1,
+        MP3 = 2,
+        OGG_OPUS = 3,
+        MULAW = 5,
+        ALAW = 6
+    }
 export interface Word {
     id: number;
     audioId: string;
@@ -34,6 +49,19 @@ export interface Word {
     }[];
   }
 
+interface SpeechesToSave {
+    id: number;
+    fileUuid: string;
+    definitions: {
+        id: number;
+        fileUuid: string;
+        examples: {
+            id: number;
+            fileUuid: string;
+        }[];
+    }[];
+}[]
+
 @Injectable()
 export class SpeechService {
 
@@ -49,72 +77,90 @@ export class SpeechService {
 
     downloadSpeechAndSave = async (titles: Word[]) => {
         const googleClient = new TextToSpeech.TextToSpeechClient();
-  
-        // TODO refactor to make more optimal connections to DB
-        
-        await Promise.all(titles.map(async ({ id: titleId, title, definitions, }) => {
+
+        const speechesToSave = await Promise.all(titles.map(async ({ id: titleId, title, definitions, }) => {
 
             const request = {
               input: { text: title },
-              voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
-              audioConfig: { audioEncoding: FileFormat.MP3.toUpperCase() },
+              voice: { languageCode: 'en-US', ssmlGender: SsmlVoiceGender.NEUTRAL },
+              audioConfig: { audioEncoding: AudioEncoding.MP3 },
             };
-    
+
             const [response] = await googleClient.synthesizeSpeech(request);
 
             const fileUuidForTitle = await this.resourceService.saveAudio(response.audioContent)
 
-            await this.titleRepository.update(
-                {
-                    id: titleId
-                }, 
-                {
-                    audioId: fileUuidForTitle
-                })
-            
-            await Promise.all(definitions.map(async ({ id: definitionId, definition, examples }) => {
+            const definitionsToSave = await Promise.all(definitions.map(async ({ id: definitionId, definition, examples }) => {
 
                 const request = {
                     input: { text: definition },
-                    voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
-                    audioConfig: { audioEncoding: FileFormat.MP3.toUpperCase() },
+                    voice: { languageCode: 'en-US', ssmlGender: SsmlVoiceGender.NEUTRAL },
+                    audioConfig: { audioEncoding: AudioEncoding.MP3 },
                   };
           
                   const [response] = await googleClient.synthesizeSpeech(request);
       
                   const fileUuidForDefinition = await this.resourceService.saveAudio(response.audioContent)
-      
-                  await this.definitionRepository.update(
-                      {
-                          id: definitionId
-                      }, 
-                      {
-                          audioId: fileUuidForDefinition
-                      })
 
-                await Promise.all(examples.map(async ({ id: exampleId, example}) => {
+                const examplesToSave = await Promise.all(examples.map(async ({ id: exampleId, example}) => {
 
                     const request = {
                         input: { text: example },
-                        voice: voiceSettings,
-                        audioConfig: { audioEncoding: FileFormat.MP3.toUpperCase() },
+                        voice: { languageCode: 'en-US', ssmlGender: SsmlVoiceGender.NEUTRAL },
+                        audioConfig: { audioEncoding: AudioEncoding.MP3 },
                       };
               
                       const [response] = await googleClient.synthesizeSpeech(request);
           
                       const fileUuidForExample = await this.resourceService.saveAudio(response.audioContent)
-          
-                      await this.exampleRepository.update(
-                          {
-                              id: exampleId
-                          }, 
-                          {
-                              audioId: fileUuidForExample
-                          })
+
+
+                    return {
+                        id: exampleId,
+                        audioId: fileUuidForExample,
+                    }
                 }))
 
+                return {
+                    id: definitionId,
+                    audioId: fileUuidForDefinition,
+                    examples: examplesToSave,
+                }
             }))
+
+            return {
+                id: titleId,
+                audioId: fileUuidForTitle,
+                definitions: definitionsToSave,
+            }
         }))
+
+        const titlesToSave = speechesToSave.map(({ id, audioId }) => ({
+            id,
+            audioId,
+        }))
+
+        await this.titleRepository.save(titlesToSave)
+
+        const definitionsToSave = speechesToSave.flatMap(({ definitions }) => {
+            return definitions.map(({ id, audioId }) => ({
+                id,
+                audioId,
+            }))
+        })
+
+        await this.definitionRepository.save(definitionsToSave)
+
+        const examplesToSave = speechesToSave.flatMap(({ definitions }) => {
+            return definitions.flatMap(({ examples }) => {
+                return examples.map(({ id, audioId }) => ({
+                    id, 
+                    audioId,
+                }))
+            })
+        })
+
+        await this.exampleRepository.save(examplesToSave)
 
     }
 
@@ -125,8 +171,8 @@ export class SpeechService {
   
         const request = {
           input: {text: text},
-          voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
-          audioConfig: {audioEncoding: 'MP3'},
+          voice: { languageCode: 'en-US', ssmlGender: SsmlVoiceGender.NEUTRAL },
+          audioConfig: { audioEncoding: AudioEncoding.MP3 },
         };
   
         const [response] = await googleTTSClient.synthesizeSpeech(request);
